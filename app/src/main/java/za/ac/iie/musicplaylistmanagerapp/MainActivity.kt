b2d106fa74441cbb288cb40ecea5fea0c2450676
ST10494318 <ST10494318@rcconnect.edu.za>
2025-06-19 07:49:12 +0000

    UI-Component

--- app/src/main/java/za/ac/iie/musicplaylistmanagerapp/MainActivity.kt
+++ app/src/main/java/za/ac/iie/musicplaylistmanagerapp/MainActivity.kt
@@ -3,13 +3,24 @@ package za.ac.iie.musicplaylistmanagerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var SongName : EditText
+    private lateinit var ArtistName : EditText
+    private lateinit var SonRating : EditText
+    private lateinit var SongComment : EditText
+    private lateinit var addButton : Button
+    private lateinit var ViewButton : Button
+    private lateinit var ExitButton : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       
+
 
     }
 }

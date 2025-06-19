f49338cc6dc11e5ef85e075ea259cc90872f1db5
ST10494318 <ST10494318@rcconnect.edu.za>
2025-06-19 07:28:53 +0000

    Initial commit

---
+++ app/src/main/java/za/ac/iie/musicplaylistmanagerapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package za.ac.iie.musicplaylistmanagerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       
+
+    }
+}
